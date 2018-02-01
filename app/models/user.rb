51bf9d1d4710aa2e7ed5693fc85b3f7fddccf050
Ryoichi Gokan <nargok@gmail.com>
2018-02-02 01:04:30 +0900

    Deviseにユーザ名でもログインできる仕組みを追加　

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,18 @@
 #  locked_at              :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string(255)
 #
 
 class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  validate :validate_name
+
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -33,5 +42,26 @@ class User < ApplicationRecord
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  attr_writer :login
+
+  def login
+    @login || name || email
+  end
+
+  def validate_name
+    errors.add(:name, :invalid) if User.where(email: name).exists?
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    conditions[:email].downcase! if conditions[:email]
+    login = conditions.delete(:login)
+
+    where(conditions.to_hash).where(
+      ['lower(name) = :value OR lower(email) = :value',
+       { value: login.downcase }]
+    ).first
   end
 end
