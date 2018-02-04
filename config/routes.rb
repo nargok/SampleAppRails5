6657c1faf31bfba36695451258de2a1fedaada7f
Ryoichi Gokan <nargok@gmail.com>
2018-02-04 17:10:54 +0900

    投稿ボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :posts, only: [:create]
+
   mount Sidekiq::Web, at: '/sidekiq'
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
